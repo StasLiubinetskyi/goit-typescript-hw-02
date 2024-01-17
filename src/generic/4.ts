68baa97ccf07418e703707dd0f10998db3da7d72
StasLiubinetskyi <StasLiubinetskyi@users.noreply.github.com>
2024-01-17 19:27:42 +0200

    Initial commit

---
+++ src/generic/4.ts
@@ -0,0 +1,19 @@
+/*
+  Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
+*/
+
+interface ComponentProps {
+  title: string;
+}
+
+class Component<T extends ComponentProps> {
+  constructor(public props: T) {}
+}
+
+class Page extends Component<ComponentProps> {
+  pageInfo(): void {
+    console.log(this.props.title);
+  }
+}
+
+export {};
---
+++ src/generic/5.ts
@@ -0,0 +1,10 @@
+/*
+  Вам потрібно реалізувати інтерфейс KeyValuePair, який описує пару ключ-значення. 
+  Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
+*/
+
+class KeyValuePair<T, K> {
+  constructor(public key: T, public value: K) {}
+}
+
+export {};
